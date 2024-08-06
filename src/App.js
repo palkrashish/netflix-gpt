3af5a075668accda6218fe2c237506a81e7449f7
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-06 23:27:30 +0530

    feat:- initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to the my Page</p>
+    </div>
+  );
+}
+
+export default App;
